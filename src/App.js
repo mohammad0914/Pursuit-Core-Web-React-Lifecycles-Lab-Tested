d1d8d09a818354a5cc567b9893612a338d1a226f
mohammad0914 <46864006+mohammad0914@users.noreply.github.com>
2022-10-02 14:12:28 +0330

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,70 @@
 import React from "react";
-import { ToastContainer } from "react-toastify";
+import Todo from Todo.js
+import { toast, ToastContainer } from "react-toastify";
 
 import "../node_modules/react-toastify/dist/ReactToastify.css";
 import "./App.css";
-
 class App extends React.Component {
-  render() {
-    return (
-      <div className="app">
-        <ToastContainer />
-        Give me something to do?!
-      </div>
-    );
-  }
+
+    constructor() {
+        this.state = {
+            todos: [],
+            input: ""
+        }
+    }
+
+    componentDidUpdate(previousProps, previousState) {
+        const { todos } = this.state;
+        if (todos.length !== previousState.todos.length) {
+            toast(`Remaining todos: ${todos.length}`);
+        }
+    }
+
+    removeToDos = (id) => {
+        const { todos } = this.state;
+        let newTodos = todos.filter((todo) => {
+            todo.id != id
+        })
+
+        this.setState({
+            todos: newTodos
+        })
+
+    }
+
+    addToDos = (e) => {
+        e.preventDefault();
+        const { todos, input } = this.state;
+        const todo = { id: todos.length, text: input }
+        this.setState({
+            todos: [...todos, todo]
+        })
+    }
+
+    mapTodoComponent = () => {
+        const { todos } = this.state
+        return todos.map((todo) => {
+            return <Todo Text={todo.Text}
+                Remove={this.removeToDos(todo.id)}
+            />
+        })
+    }
+
+    handeInput = (e) => {
+        this.setState({ input: e.target.value })
+    }
+
+    render() {
+        return (<>
+            <form onSubmit={this.addToDos}>
+                <input onChange={this.handeInput} ></input>
+            </form>
+            <div>
+                {this.mapTodoComponent()}
+            </div>
+        </>);
+    }
+
 }
 
 export default App;
