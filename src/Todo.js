f597185a5e157d731f9308771e6e0c8a58ecbd70
mohammad0914 <46864006+mohammad0914@users.noreply.github.com>
2022-10-02 14:09:56 +0330

    Create Todo.js

---
+++ src/Todo.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+class Todo extends React.Component {
+
+    constructor() {
+
+    }
+
+    componentDidMount() {
+        toast.success("");
+    }
+
+    componentWillUnmount() {
+        toast.error("");
+    }
+
+    render() {
+        const { Text, Remove } = this.props
+        return (
+            <div>
+                <input>{Text}</input>
+                <button onClick={Remove()}>--</button>
+            </div>
+        );
+    }
+
+}
